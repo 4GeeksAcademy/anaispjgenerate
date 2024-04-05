c137c070341c9b08f464e1bb580a86a31d73be57
Anais Jimenez <158285035+anaispj@users.noreply.github.com>
2024-04-05 18:32:13 +0000

    cambios

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domain = [".com", ".es", ".net", ".uk"];
+
+  const combinaciones = pronoun.map(p => {
+    return adj.map(a => {
+      return noun.map(n => {
+        return domain.map(d => {
+          return " " + p + a + n + d;
+        });
+      });
+    });
+  });
+
+  let websites = document.querySelector("#doms");
+
+  websites.innerHTML = combinaciones;
 };
